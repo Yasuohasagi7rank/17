bc4d4ea5630327054e1eac46d24395fc20dc33dc
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:09:33 +0300

    Add files via upload

---
+++ 6.55/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6._55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a;
+            int c = 0;
+            while (a > 0)
+            {
+                c *= 10;
+                c = c + a % 10;
+                a /= 10;
+            }
+            if (c == b)
+                Console.WriteLine("Палиндром");
+            else
+                Console.WriteLine("Не палиндром");
+        }
+    }
+}
